fc73f43196d81b3ed4b02887fb61ae0f66705d99
Thavanish <94302592+Thavanish@users.noreply.github.com>
2024-10-18 23:02:10 +0530

    Update and rename server.sh to functions.sh

--- File/functions.sh
+++ File/functions.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 # hey there !
+
 RAM="1024" # this is to set the ram use the MB only
 JAR='files/server.jar' # this is the server's jar file location change it as you please or leave it as is for default fabric server 1.21.1
+NAME='TOPG' # the name for the server's title
 
 ## after this this area its danger zone !
 # i am not responsible for any damages done by edit this script !
 
 # the fuctions file 
-source /files/func/func.sh
+source /files/server.sh
+# defining the functions 
 
-# starting the script 
-
+function ui {
+   dialog 
+}
