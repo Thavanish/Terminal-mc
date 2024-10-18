9353c8df2006edae814ad1e6069d14eb4050f8b1
Thavanish <94302592+Thavanish@users.noreply.github.com>
2024-10-18 22:45:43 +0530

    Create Server.sh

---
+++ mc-server-files/functions/Server.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# hey there !
+RAM="1024" # this is to set the ram use the MB only
+JAR='files/server.jar' # this is the server's jar file location change it as you please or leave it as is for default fabric server 1.21.1
+
+## after this this area its danger zone !
+# i am not responsible for any damages done by edit this script !
+
+# the fuctions file 
+source /files/func/func.sh
+
+# starting the script 
+
